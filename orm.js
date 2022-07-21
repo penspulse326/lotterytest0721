fe99907cb81c6f0bfd730277a6938aebd9d93b3a
SHIN <akiyachiumi@gmail.com>
2022-07-21 17:08:03 +0800

    test

--- orm.js
+++ orm.js
@@ -1,12 +1,10 @@
 const db = require('./models');
 
-const { Award } = db;
+const { User } = db;
 
-Award.create({
-  name: '鮮蔬寶堡',
-  content: '好吃A漢堡',
-  url: 'https://taiwan.sharelife.tw/tw-feat-store-img/59175/f424280621563310.jpg',
-  rate: 5,
+User.create({
+  username: 'test',
+  password: '0000',
 }).then(() => {
   console.log('Done!');
 });
